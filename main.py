cdf59309459f51e096115757b24609543db7d485
luke <lukeabela56@gmail.com>
2024-08-09 23:17:21 +0200

    We are now indexing data

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+from tqdm import tqdm
+from dotenv import dotenv_values
+
+from src.elastic import ELK
 from src.nlp import process_dict
 from src.utils import get_filepaths, get_text_from_filepath, create_query_response_dict, dicts_to_csv, dicts_to_json
-from tqdm import tqdm
+
 
 ROOT_PATH: str = "funderpro_faqs"
 CSV_PATH: str = "artifacts/data.csv"
 JSON_PATH: str = "artifacts/data.json"
+ENV_PATH: str = ".env"
 
 def main() -> int:
+
+    config = dotenv_values(ENV_PATH)  
+    elk = ELK(config=config)
+    elk.establish_es_connection()
+
+    elk.delete_index()
+    elk.create_index()
 
     filepaths: list = list(get_filepaths(ROOT_PATH))
     dicts: list = []
@@ -17,6 +29,10 @@ def main() -> int:
         text: str = get_text_from_filepath(filepath)
         dict = create_query_response_dict(text, id=i)
         dict = process_dict(dict)
+
+        elk.index_document(
+            document=dict
+        )
         dicts.append(dict)
     
     dicts_to_csv(dicts, csv_path=CSV_PATH)
--- src/elastic.py
+++ src/elastic.py
@@ -1,30 +1,46 @@
 from elasticsearch import Elasticsearch
-from dotenv import dotenv_values
 
-config = dotenv_values("../.env")  
+class ELK():
+    def __init__(self, config):
 
-config["ELASTICSEARCH_HOST"]
-# Define connection parameters
-ELASTICSEARCH_HOST = config["ELASTICSEARCH_HOST"]
-ELASTICSEARCH_USERNAME = config["ELASTICSEARCH_USERNAME"]
-ELASTICSEARCH_PASSWORD = config["ELASTICSEARCH_PASSWORD"]
+        # Define connection parameters
+        self.host: str = config["ELASTICSEARCH_HOST"]
+        self.username: str = config["ELASTICSEARCH_USERNAME"]
+        self.password: str = config["ELASTICSEARCH_PASSWORD"]
+        self.index: str = config["INDEX"]
 
-# Create an Elasticsearch client instance
-es = Elasticsearch(
-    hosts=[ELASTICSEARCH_HOST],
-    basic_auth=(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD),
-)
 
-# Check if Elasticsearch is up and running
-def check_connection():
-    try:
-        # Perform a simple health check
-        response = es.cluster.health()
-        print("Elasticsearch cluster health:", response['status'])
-    except Exception as e:
-        print("Error connecting to Elasticsearch:", e)
+    def establish_es_connection(self):
+        # Create an Elasticsearch client instance
+        self.es = Elasticsearch(
+            hosts=[self.host],
+            basic_auth=(self.username, self.password)
+        )
 
-# Example usage
-if __name__ == "__main__":
-    check_connection()
+    # Check if Elasticsearch is up and running
+    def validate_es_connection(self,):
+        try:
+            # Perform a simple health check
+            response = self.es.cluster.health()
+            print("Elasticsearch cluster health:", response['status'])
+        except Exception as e:
+            print("Error connecting to Elasticsearch:", e)
 
+    def create_index(self):
+        # Check if an index exists
+        if self.es.indices.exists(index=self.index): 
+            print(f"Index {self.index} exists")
+        else:
+            self.es.indices.create(index=self.index)
+            print(f"Index {self.index} created")
+
+    def delete_index(self):
+        # Delete an index
+        if self.es.indices.exists(index=self.index): 
+            self.es.indices.delete(index=self.index)
+            print(f"Index {self.index} deleted")
+        else:
+            print(f"Index {self.index} doesn't exist")
+
+    def index_document(self, document: dict):
+        self.es.index(index=self.index, id=dict["id"], document=document)
